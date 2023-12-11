dbcfdef9947483ca463e6d521c7fbfffa34e77ea
keerthisravani <keerthisravani1202@gmail.com>
2023-12-11 09:25:26 +0530

    create header component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Signin from './pages/Signin';
 import SignOut from './pages/SignOut';
 import Profile from './pages/Profile';
 import About from './pages/About';
+import Header from './components/Header';
 
 export default function App() {
   return <BrowserRouter>
+  <Header />
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/sign-in" element={<Signin />} />
     <Route path="/sign-up" element={<SignOut />} /> 
     <Route path="/profile" element={<Profile/>} />
     <Route path="/about" element={<About />} />
-
+   
   </Routes>
   </BrowserRouter>
 }
--- src/pages/Profilee.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Profilee() {
-  return (
-    <div>Profilee</div>
-  )
-}
